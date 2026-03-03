8a2a9723bd853d59ec8ebc13fa0508c9aabbcaf6
Afisphbl <abuabdurehman0308@gmail.com>
2026-03-03 13:19:23 +0300

    Refactor Details component to display selected movie information with enhanced layout

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -7,10 +7,34 @@ function Details() {
   const { selectedMovieID } = useSelectedMovieID();
   const { selectedMovie } = useSelectedMovie(selectedMovieID);
   console.log(selectedMovie);
-  return selectedMovie ? (
-    <p>{selectedMovie.title}</p>
-  ) : (
-    <p>No movie selected</p>
+  return (
+    selectedMovie && (
+      <section className={classes.details}>
+        <header>
+          <img
+            src={selectedMovie.Poster}
+            alt={`${selectedMovie.Title} poster`}
+          />
+          <div className={classes["details-overview"]}>
+            <h2>{selectedMovie.Title}</h2>
+            <p>
+              {selectedMovie.Released} &bull; {selectedMovie.Runtime}
+            </p>
+            <p>{selectedMovie.Genre}</p>
+            <p>
+              <span>⭐️</span>
+              {selectedMovie.imdbRating} IMDB rating
+            </p>
+          </div>
+        </header>
+
+        <section>
+          <p>{selectedMovie.Plot}</p>
+          <p>{selectedMovie.Actors}</p>
+          <p>{selectedMovie.Director}</p>
+        </section>
+      </section>
+    )
   );
 }
 
